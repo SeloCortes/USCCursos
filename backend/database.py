531ebc452879ded9e9c478e38d13f031648eb016
SeloCortes <you@example.com>
2025-11-27 17:03:48 -0500

    fix x2

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = "postgresql://uscadminpostgres:yBjPWmGYtVJ2IhwcvoXBzAKtRgvR8QOI@dpg-d4hj6pruibrs73dlep60-a/cursos_usc_bienestar_db_eo0o" # URL para conexion en la nube Render
+URL_DATABASE = "postgresql://uscadminpostgres:P7D2niA4WGkiVUA8ocPMea8ezErcv2qu@dpg-d4kcj0i4d50c73de0s0g-a/cursos_usc_bienestar_db_2frf" # URL para conexion en la nube Render
 
 
 """URL_DATABASE = "postgresql://postgres:postgres123@localhost:5432/cursos_USC_Bienestar_db" # URL para conexion local"""
